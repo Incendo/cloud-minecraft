05bb20b4ea1eade214c37ca9b0e1976bbfa7aa61
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-01-23 22:05:09 -0700

    Move adventure from api to compileOnlyApi
    see comments for reasoning

--- cloud-minecraft-extras/build.gradle.kts
+++ cloud-minecraft-extras/build.gradle.kts
@@ -5,7 +5,14 @@ plugins {
 
 dependencies {
     api(libs.cloud.core)
-    api(libs.adventureApi)
-    api(libs.adventureTextSerializerPlain)
+
+    // We expect these to either be
+    //   1) Provided by the platform at runtime
+    //     or
+    //   2) Shaded/included by the user either explicitly or as a
+    //      transitive dependency of adventure-platform
+    compileOnlyApi(libs.adventureApi)
+    compileOnlyApi(libs.adventureTextSerializerPlain)
+    // Only needed for features that explicitly mention MiniMessage
     compileOnlyApi(libs.minimessage)
 }
