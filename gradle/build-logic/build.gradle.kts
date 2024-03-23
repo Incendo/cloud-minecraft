3584b9119a81136e616558188d25426e0f544d82
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-03-23 14:58:41 -0700

    build: Remove no longer needed build dependency

--- gradle/build-logic/build.gradle.kts
+++ gradle/build-logic/build.gradle.kts
@@ -13,8 +13,6 @@ repositories {
 }
 
 dependencies {
-    // loom needs this version of asm, for some reason we have an older one on the classpath without this
-    implementation("org.ow2.asm:asm:9.6")
     implementation(libs.cloud.build.logic)
 
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
