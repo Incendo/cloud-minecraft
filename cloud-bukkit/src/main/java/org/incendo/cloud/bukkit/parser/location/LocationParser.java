57fee80af625a5715ade4c9df7124522ca7c4743
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2024-05-27 21:19:30 +0200

    fix compilation due to missing javadoc, add missing api, fix empty input for `MIXED_LOCAL_ABSOLUTE` reason

--- cloud-bukkit/src/main/java/org/incendo/cloud/bukkit/parser/location/LocationParser.java
+++ cloud-bukkit/src/main/java/org/incendo/cloud/bukkit/parser/location/LocationParser.java
@@ -99,7 +99,7 @@ public final class LocationParser<C> implements ArgumentParser<C, Location>, Blo
                     )
             );
         }
-
+        final CommandInput originalInput = commandInput.copy();
         final LocationCoordinate[] coordinates = new LocationCoordinate[3];
         for (int i = 0; i < 3; i++) {
             if (commandInput.peekString().isEmpty()) {
@@ -147,7 +147,7 @@ public final class LocationParser<C> implements ArgumentParser<C, Location>, Blo
                     new LocationParseException(
                             commandContext,
                             LocationParseException.FailureReason.MIXED_LOCAL_ABSOLUTE,
-                            ""
+                            originalInput.remainingInput()
                     )
             );
         }
@@ -254,7 +254,16 @@ public final class LocationParser<C> implements ArgumentParser<C, Location>, Blo
 
     public static final class LocationParseException extends ParserException {
 
+        private final String input;
+        private final FailureReason reason;
 
+        /**
+         * Construct a new LocationParseException
+         *
+         * @param context Command context
+         * @param reason  Failure reason
+         * @param input   Input
+         */
         public LocationParseException(
                 final @NonNull CommandContext<?> context,
                 final @NonNull FailureReason reason,
@@ -266,6 +275,26 @@ public final class LocationParser<C> implements ArgumentParser<C, Location>, Blo
                     reason.caption(),
                     CaptionVariable.of("input", input)
             );
+            this.input = input;
+            this.reason = reason;
+        }
+
+        /**
+         * Get the supplied input
+         *
+         * @return String value
+         */
+        public @NonNull String input() {
+            return this.input;
+        }
+
+        /**
+         * Get the failure reason
+         *
+         * @return Failure reason
+         */
+        public @NonNull FailureReason reason() {
+            return this.reason;
         }
 
 
