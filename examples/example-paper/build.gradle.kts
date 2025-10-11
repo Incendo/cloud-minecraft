57282718f6db7ca1d3dac6f7f56ad81bf9f18d88
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-10-11 16:18:56 -0700

    Update shadow config

--- examples/example-paper/build.gradle.kts
+++ examples/example-paper/build.gradle.kts
@@ -37,6 +37,10 @@ tasks {
         relocate("net.fabricmc.mappingio", "org.incendo.cloud.example.mappingio")
 
         mergeServiceFiles()
+        // Needed for mergeServiceFiles to work properly in Shadow 9+
+        filesMatching("META-INF/services/**") {
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
 
         manifest {
             attributes("paperweight-mappings-namespace" to "mojang")
